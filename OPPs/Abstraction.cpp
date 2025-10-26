5f9f67f14848e14e6d28b8ccb7f7f2d7630fc126
Mohammad Arzaul Haque <arzaulhaqu786@gmail.com>
2025-10-26 23:59:14 +0530

    change in abstract code
    Updated comments to clarify the purpose of the abstract class.

--- OPPs/Abstraction.cpp
+++ OPPs/Abstraction.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 /*
-Abstract class --> 
+Abstract class -------> 
 1. Act as an interface for the outside world to operate the car. 
 2. This abstract class tells 'WHAT' all it can do rather then 'HOW' it does that.
 3. Since this is an abstract class we cannot directly create Objects of this class. 
@@ -17,6 +17,7 @@ pedals/buttons/ steer the wheel etc. You dont need to know how these things work
 also they are hidden under the hood.
 6. This Class 'Car' denotes that (pedals/buttons/steering wheel etc). 
 */
+//Abstract class
 class Car {
 public:
     virtual void startEngine() = 0;
